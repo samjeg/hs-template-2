6ca99aec77ee4846f6125c6e10f05a72339dc8cd
Samuel Jegede <sam.jegede@outlook.com>
2021-02-16 01:10:57 +0000

    updated detail page

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ $(document).ready(function(){
 	$recipes = $(".recipes .recipe-item");
 	$recipes_size = $recipes.length;
 	$footer = $("footer");
-     
-    
-    console.log("Hello World: " + $recipes_size);
 
     /* fixes the footer to the bottom of the page making sure
     not to overlap the content */

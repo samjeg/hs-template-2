40f50d67c73293b49355551f96965b5d1a8e7be3
Samuel Jegede <sam.jegede@outlook.com>
2021-07-11 12:36:45 +0100

    updated recipe list title and recipe item

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ $(document).ready(function(){
 	$recipes = $(".recipes .recipe-item");
 	$recipes_length = $recipes.length;
 	$footer = $("footer");
-
+    
+    // keep footer recipes list 
 	if ($recipes_length > 3) {
 		$footer.css("position", "relative");
 	} else {
 		$footer.css("position", "fixed");
 	}
 
-	if ($recipes_length > 1) {
-		$(".recipes .recipe-item:last-child").css("border-bottom", "hidden");
-	}
+	// remove border from last recipe item element
+	$(".recipes .recipe-item:last-child").css("border-bottom", "hidden");
+	
 });
